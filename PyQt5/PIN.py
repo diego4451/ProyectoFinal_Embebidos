f06f3551ef48ddf65599937915ee452b781d9a30
Diego <diego.moravalverde@gmail.com>
2020-08-10 17:36:30 -0600

    Primera parte de la integración

--- PyQt5/PIN.py
+++ PyQt5/PIN.py
@@ -11,8 +11,150 @@
 
 import sys
 import pyautogui as gui
+import cv2
+import numpy as np
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Access import Ui_MainWindow
+
+def rescale_frame(frame, wpercent=130, hpercent=130):
+    width = int(frame.shape[1] * wpercent / 100)
+    height = int(frame.shape[0] * hpercent / 100)
+    return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
+
+
+def contours(hist_mask_image):
+    gray_hist_mask_image = cv2.cvtColor(hist_mask_image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray_hist_mask_image, 0, 255, 0)
+    cont, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return cont
+
+def draw_rect(frame):
+    rows, cols, _ = frame.shape
+    global total_rectangle, hand_rect_one_x, hand_rect_one_y, hand_rect_two_x, hand_rect_two_y
+
+    hand_rect_one_x = np.array(
+        [6 * rows / 20, 6 * rows / 20, 6 * rows / 20, 9 * rows / 20, 9 * rows / 20, 9 * rows / 20, 12 * rows / 20,
+         12 * rows / 20, 12 * rows / 20], dtype=np.uint32)
+
+    hand_rect_one_y = np.array(
+        [9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20,
+         10 * cols / 20, 11 * cols / 20], dtype=np.uint32)
+
+    hand_rect_two_x = hand_rect_one_x + 10
+    hand_rect_two_y = hand_rect_one_y + 10
+
+    for i in range(total_rectangle):
+        cv2.rectangle(frame, (hand_rect_one_y[i], hand_rect_one_x[i]),
+                      (hand_rect_two_y[i], hand_rect_two_x[i]),
+                      (0, 255, 0), 1)
+
+    return frame
+
+
+def hand_histogram(frame):
+    global hand_rect_one_x, hand_rect_one_y
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    roi = np.zeros([90, 10, 3], dtype=hsv_frame.dtype)
+
+    for i in range(total_rectangle):
+        roi[i * 10: i * 10 + 10, 0: 10] = hsv_frame[hand_rect_one_x[i]:hand_rect_one_x[i] + 10,
+                                          hand_rect_one_y[i]:hand_rect_one_y[i] + 10]
+
+    hand_hist = cv2.calcHist([roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    return cv2.normalize(hand_hist, hand_hist, 0, 255, cv2.NORM_MINMAX)
+
+
+def hist_masking(frame, hist):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
+
+    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
+    cv2.filter2D(dst, -1, disc, dst)
+
+    ret, thresh = cv2.threshold(dst, 150, 255, cv2.THRESH_BINARY) #ret, thresh = cv2.threshold(dst, 150, 255, cv2.THRESH_BINARY)
+
+    #thresh = cv2.dilate(thresh, None, iterations=5)
+
+    thresh = cv2.merge((thresh, thresh, thresh))
+
+    return cv2.bitwise_and(frame, thresh)
+
+
+def centroid(max_contour):
+    moment = cv2.moments(max_contour)
+    if moment['m00'] != 0:
+        cx = int(moment['m10'] / moment['m00'])
+        cy = int(moment['m01'] / moment['m00'])
+        return cx, cy
+    else:
+        return None
+
+def draw_circles(frame, traverse_point):
+    if traverse_point is not None:
+        for i in range(len(traverse_point)):
+            cv2.circle(frame, traverse_point[i], 5, [0, 255, 255], -1)
+
+def manage_image_opr(frame, hand_hist):
+    hist_mask_image = hist_masking(frame, hand_hist)
+
+    hist_mask_image = cv2.erode(hist_mask_image, None, iterations=2)
+    hist_mask_image = cv2.dilate(hist_mask_image, None, iterations=2)
+
+    contour_list = contours(hist_mask_image)
+    max_cont = max(contour_list, key=cv2.contourArea)
+
+    cnt_centroid = centroid(max_cont)
+    cv2.circle(frame, cnt_centroid, 5, [255, 0, 255], -1)
+
+    if max_cont is not None:
+        hull = cv2.convexHull(max_cont, returnPoints=False)
+        defects = cv2.convexityDefects(max_cont, hull)
+        #far_point = farthest_point(defects, max_cont, cnt_centroid)
+        print("Centroid : " + str(cnt_centroid))
+
+def tracking(hand_hist):
+
+    capture = cv2.VideoCapture(0)
+
+    pressed_key = cv2.waitKey(42)
+    _, frame = capture.read()
+    frame = cv2.flip(frame, 1)
+
+    manage_image_opr(frame, hand_hist)
+
+    capture.release()
+
+def cal():
+
+    hand_hist = None
+    traverse_point = []
+    total_rectangle = 9
+    hand_rect_one_x = None
+    hand_rect_one_y = None
+
+    hand_rect_two_x = None
+    hand_rect_two_y = None
+
+    global hand_hist
+    capture = cv2.VideoCapture(0)
+
+    while capture.isOpened():
+        pressed_key = cv2.waitKey(42)
+        _, frame = capture.read()
+        frame = cv2.flip(frame, 1)
+
+        if pressed_key & 0xFF == ord('z'):
+            hand_hist = hand_histogram(frame)
+            cv2.destroyAllWindows()
+            capture.release()
+            return hand_hist
+        else:
+            frame = draw_rect(frame)
+        imagen1=rescale_frame(frame)
+        cv2.imshow("Live Feed", imagen1)
+        #cv2.imwrite("imagen.jpg", imagen1)
 
 class Welcome(object):
     def openWindow(self):
@@ -138,10 +280,12 @@ class Welcome(object):
         self.string  = "Enter PIN here"
         self.textEdit.setPlainText(self.string)
         self.openWindow()
-        
+
 
 if __name__ == "__main__":
     import sys
+    cal()
+    
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Welcome()
